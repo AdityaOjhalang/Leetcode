3fa619ab7a3d29d94252be1c8d8435c2bd4e5446
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-04-03 19:59:33 -0400

    Time: 106 ms (45.04%), Space: 130.7 MB (14.69%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -3,7 +3,6 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
 
         int n = gas.size();
-        vector<int> diff(n);
         int gasSum = accumulate(gas.begin(), gas.end(), 0);
         int costSum = accumulate(cost.begin(), cost.end(), 0);
 
@@ -14,11 +13,7 @@ public:
             return -1;
         } else {
             for (int i = 0; i < n; i++) {
-                diff[i] = gas[i] - cost[i];
-            }
-
-            for (int i = 0; i < n; i++) {
-                total += diff[i];
+                total += gas[i] - cost[i];
                 if (total < 0) {
                     total = 0;
                     start = i + 1;
