706c1303bb1f8c749a301ed2d3ce534bc151fba1
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-04-23 20:15:30 -0400

    Time: 3 ms (41.79%) | Memory: 7.4 MB (95.67%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t pos = haystack.find(needle);
+
+        // If find returns std::string::npos, it means the substring was not
+        // found.
+        if (pos == std::string::npos) {
+            return -1; // Return -1 if the substring is not found
+        } else {
+            return static_cast<int>(
+                pos); // Cast size_t to int and return the position
+        }
+    }
+};
