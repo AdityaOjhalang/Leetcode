14e003da4dd9cd486cbf0aeb987835f0794df7b3
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-03 14:21:17 -0700

    Time: 4 ms (46.33%) | Memory: 7.6 MB (54.88%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -7,7 +7,7 @@ public:
 
             if(s[i] == ' ' && count==0) continue;
             if(s[i] == ' ' & count>0) return count;
-            if(s[i] != ' ') count++;
+            else count++;
         }
         return count;
     }
