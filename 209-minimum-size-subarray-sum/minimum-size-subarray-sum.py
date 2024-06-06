fd53cb10a8abbb9821c7e69a9a848e75a3825b45
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-05 18:28:52 -0700

    Time: 183 ms (94.09%) | Memory: 30 MB (94.88%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        low = 0
+        size = n+100
+        curr = 0
+        for high in range(n):
+            curr += nums[high]
+            while(curr >= target):
+                size = min(size, high-low+1)
+                curr -= nums[low]
+                low +=1
+        if size == n+100:
+            return 0
+        else:    
+            return size
+                
+                
+
+
