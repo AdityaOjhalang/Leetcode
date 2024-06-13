08aa5e52263e8e9910d01b87417cc863349d4b0c
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-13 16:33:35 -0700

    Time: 60 ms (60.38%) | Memory: 17.6 MB (59.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        vals = {value: index for index, value in enumerate(nums)}
+        #vals = {value: index for index, value in enumerate(nums)}
+        vals = {}
+        for i in range(len(nums)):
+            vals[nums[i]] = i
         n = len(nums)
         for i in range(n):
             v = target - nums[i]
