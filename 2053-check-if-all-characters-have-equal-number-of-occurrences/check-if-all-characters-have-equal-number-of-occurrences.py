4de04dfbe7762b3342c943a1acf4a647ef5643aa
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-17 16:39:42 -0700

    Time: 38 ms (59.02%) | Memory: 16.5 MB (65.78%) - LeetSync

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+
+        words = Counter(s)
+        tocheck = words[s[0]]
+
+        for w in words:
+            if(words[w] != tocheck):
+                return False
+        return True
