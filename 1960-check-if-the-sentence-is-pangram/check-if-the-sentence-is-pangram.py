fb068fb330e3a5e5dc4af1775a7d55a4dcf7ed4f
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-17 13:41:11 -0700

    Time: 42 ms (12.67%) | Memory: 16.4 MB (80.18%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,21 +1,10 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        dic = "abcdefghijklmnopqrstuvwxyz"
-        hmap = {}
-        
-        for i in range(len(dic)):
-            hmap[dic[i]] = i
-        
-        count = 0 
 
-        for i in range(len(sentence)):
-            curr = sentence[i]
-            if curr in hmap :
-                del hmap[curr]
-
-        if len(hmap) == 0 :
-            return True
-        else:
-            return False
+            hset = set(sentence)
+            if len(hset) != 26:
+                return False
+            else:
+                return True
                 
         
