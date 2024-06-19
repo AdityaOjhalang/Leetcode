5525ad38ab45a96d9a1b2ca5fa2449d2a0e36f08
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-18 21:41:42 -0700

    Time: 50 ms (80.31%) | Memory: 16.5 MB (98.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,14 +3,15 @@ class Solution:
         charindx = {}
         start = 0
         maxsize = 0
+        n = len(s)
 
         for end in range(len(s)):
             curr = s[end]
 
             if curr in charindx and charindx[curr] >= start:
-                start = charindx[curr]+1
+                start = charindx[curr] +1
             
-            charindx[curr] = end 
-            maxsize = max(maxsize , end-start+1)
-        
+            charindx[curr] = end
+            maxsize = max(maxsize , end - start + 1 )
         return maxsize
+
