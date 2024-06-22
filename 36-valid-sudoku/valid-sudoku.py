64ac5a03770fe3b71c6f91b98cea2f1bb33fc2b6
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-22 13:35:46 -0700

    Time: 93 ms (49.52%) | Memory: 16.6 MB (46.40%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -5,18 +5,16 @@ class Solution:
         box = collections.defaultdict(set)
 
         for row in range(9):
-            for column in range(9):
-                if board[row] [column] == ".":
+            for col in range(9):
+                curr = board[row][col]
+                if(curr == "."):
                     continue
-                if (
-                    board[row][column] in rows[row]
-                    or board[row][column] in cols[column]
-                    or board[row][column] in box[(row // 3, column // 3)]
-                ):
+
+                if curr in rows[row] or curr in cols[col] or curr in box[row//3,col//3]:
                     return False
                 
-                rows[row].add(board[row][column])
-                cols[column].add(board[row][column])
-                box[(row // 3,column // 3)].add(board[row][column])
-
+                rows[row].add(curr)
+                cols[col].add(curr)
+                box[row//3,col//3].add(curr)
+        
         return True
