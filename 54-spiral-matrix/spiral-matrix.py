6dc3fb9ac6883a25f4a82bfa42f1831b2b36d2fe
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-06-22 14:17:07 -0700

    Time: 30 ms (87.41%) | Memory: 16.6 MB (43.53%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,33 +1,37 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
+
         res = []
-        left , right = 0 , len(matrix[0])
-        top , bottom = 0 , len(matrix)
+
+        top, bottom = 0, len(matrix)  # These are rows
+        left, right = 0, len(matrix[0])  # These are cols
 
         while left < right and top < bottom:
-            #going right
-            for j in range(left,right):
+
+            # going right
+            for j in range(left, right):
                 res.append(matrix[top][j])
             top += 1
-
-            #going down
-            for j in range(top,bottom):
-                res.append(matrix[j][right-1])
+            if not (left < right  and top < bottom) :
+                break
+            # going down
+            for j in range(top, bottom):
+                res.append(matrix[j][right - 1])
             right -= 1
 
             if not (left < right  and top < bottom) :
                 break
-
-            #going left
-            for j in range(right - 1,left - 1, -1):
-                res.append(matrix[bottom -1][j])
-            bottom -= 1
-
-            #going upar
-            for j in range(bottom - 1 , top - 1, -1 ):
+            # going left
+            for j in range(right-1,left-1,-1):
+                res.append(matrix[bottom-1][j])
+            bottom -=1
+            if not (left < right  and top < bottom) :
+                break
+            # going up
+            for j in range(bottom-1,top-1,-1):
                 res.append(matrix[j][left])
-            left += 1
+            left+=1
+            
+            
 
         return res
-
