7704a9fb8f97e6e4e7adf056a1658a88cc69f022
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-09-28 20:56:49 -0400

    Time: 258 ms (41.88%), Space: 27.2 MB (12.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def valid(row, col):
-            return 0 <= row < m and 0 <= col < n and grid[row][col] == "1"
+            return 0 <= row < rows and 0 <= col < cols and grid[row][col] == "1"
 
-        def dfs(row, col):
+        def dfsNeigh(row, col):
             for x, y in directions:
-                next_r, next_c = row + x, col + y
-                if valid(next_r,next_c) and (next_r,next_c) not in seen:
-                    seen.add((next_r,next_c))
-                    dfs(next_r,next_c)
-
-        ans = 0
-        m = len(grid)
-        n = len(grid[0])
+                nextrow, nextcol = row + x, col + y
+                if valid(nextrow,nextcol) and (nextrow,nextcol) not in seen:
+                    seen.add((nextrow,nextcol))
+                    dfsNeigh(nextrow,nextcol)
+        
+        rows = len(grid)
+        cols = len(grid[0])
         seen = set()
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        res = 0
 
-        for i in range(m):
-            for j in range(n):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1" and (i,j) not in seen:
                     seen.add((i,j))
-                    ans+=1
-                    dfs(i,j)
-        return ans
-
+                    res +=1
+                    dfsNeigh(i,j)
+        return res
