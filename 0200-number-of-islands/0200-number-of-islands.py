2d93a796ce3a6c28a41222c686d78782e56f53b5
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-09-28 20:57:59 -0400

    Time: 280 ms (24.32%), Space: 27.3 MB (12.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,8 +1,9 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        #helper function to check whether the position is valid and is an island or not 
         def valid(row, col):
             return 0 <= row < rows and 0 <= col < cols and grid[row][col] == "1"
-
+        #recrusive DFS to find all neighbors in the set
         def dfsNeigh(row, col):
             for x, y in directions:
                 nextrow, nextcol = row + x, col + y
@@ -13,9 +14,10 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         seen = set()
+        #going in all directions to find neighbors
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         res = 0
-
+        #running the funciton
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1" and (i,j) not in seen:
