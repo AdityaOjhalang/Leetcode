d83756b49ec573f24384ed0ba3c9d6a7e0be19ea
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-10-01 10:38:17 -0400

    Time: 63 ms (80.34%), Space: 17.1 MB (61.68%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        def dfs(node):
+            for neigh in rooms[node]:
+                if neigh not in seen:
+                    seen.add(neigh)
+                    dfs(neigh)
+        
+        seen = {0}
+        dfs(0)
+        return len(rooms) == len(seen)
