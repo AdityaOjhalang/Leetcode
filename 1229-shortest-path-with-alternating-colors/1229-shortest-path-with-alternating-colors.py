a453bcd72790dc84a2a01f6967de5cfd164bb4c2
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-10-21 20:45:32 -0400

    Time: 6 ms (97.07%), Space: 16.9 MB (11.99%) - LeetHub

--- 1229-shortest-path-with-alternating-colors/1229-shortest-path-with-alternating-colors.py
+++ 1229-shortest-path-with-alternating-colors/1229-shortest-path-with-alternating-colors.py
@@ -6,22 +6,25 @@ class Solution:
         BLUE = 1
         
         graph = {RED: defaultdict(list), BLUE: defaultdict(list)}
-        for x, y in redEdges:
+
+        for x , y in redEdges:
             graph[RED][x].append(y)
-        for x, y in blueEdges:
+        for x , y in blueEdges:
             graph[BLUE][x].append(y)
         
         ans = [float("inf")] * n
-        queue = deque([(0, RED, 0), (0, BLUE, 0)])
-        seen = {(0, RED), (0, BLUE)}
-        
+
+        queue = deque([(0,RED,0),(0,BLUE,0)])
+
+        seen = {(0,RED),(0,BLUE)}
+
         while queue:
-            node, color, steps = queue.popleft()
-            ans[node] = min(ans[node], steps)
-            
-            for neighbor in graph[color][node]:
-                if (neighbor, 1 - color) not in seen:
-                    seen.add((neighbor, 1 - color))
-                    queue.append((neighbor, 1 - color, steps + 1))
-        
+            node,color,steps = queue.popleft()
+            ans[node] = min(steps,ans[node])
+
+            for neigh in graph[color][node]:
+                if (neigh,1-color) not in seen:
+                    seen.add((neigh,1-color))
+                    queue.append((neigh,1-color,steps+1))
+
         return [x if x != float("inf") else -1 for x in ans]
