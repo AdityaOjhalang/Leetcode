878af2ff2a5ac7dd83c6cffbac63da9926dc9769
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-10-23 20:03:39 -0400

    Time: 0 ms (100%), Space: 16.7 MB (70.29%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,35 +1,37 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    def calcEquation(
+        self, equations: List[List[str]], values: List[float], queries: List[List[str]]
+    ) -> List[float]:
 
-        graph = defaultdict(dict)
+        neighbors = defaultdict(list)
 
         for i in range(len(equations)):
-            x,y = equations[i]
-            graph[x][y] = values[i]
-            graph[y][x] = 1/values[i]
-        
-        def querycalc(start,end):
-            if start not in graph or end not in graph:
+            x, y = equations[i]
+            val = values[i]
+            neighbors[x].append((y, val))
+            neighbors[y].append((x, 1/val))
+
+
+        def querycalc(start, end):
+            if start not in neighbors or end not in neighbors:
                 return -1
-            
+
             seen = {start}
-            queue = deque([(start,1)])
+            queue = deque([(start, 1)])
 
             while queue:
-                node,ratio = queue.popleft()
+                node, ratio = queue.popleft()
                 if node == end:
                     return ratio
-                
-                for neigh in graph[node]:
+
+                for neigh,weight in neighbors[node]:
                     if neigh not in seen:
                         seen.add(neigh)
-                        queue.append((neigh,ratio*graph[node][neigh]))
+                        queue.append((neigh, ratio * weight))
             return -1
-        
-        ans = []
-        for i,val in enumerate(queries):
-            n,d = val
-            ans.append(querycalc(n,d))
-        return ans
-                    
 
+        ans = []
+        for i, val in enumerate(queries):
+            n, d = val
+            ans.append(querycalc(n, d))
+        return ans
