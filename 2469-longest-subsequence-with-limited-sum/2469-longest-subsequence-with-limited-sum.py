7e6152dde7686a69757018a75681e589ec0b2236
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-09 19:00:03 -0500

    Time: 0 ms (100%), Space: 16.9 MB (47.57%) - LeetHub

---
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        def bs(arr, tar):
+            left, right = 0, len(arr) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if arr[mid] > tar:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return left  # Left is the insertion point for tar
+        nums.sort()
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+        res = []
+        for q in queries:
+            indx = bisect.bisect_right(nums,q)
+            res.append(indx)
+        return res
+
+        
