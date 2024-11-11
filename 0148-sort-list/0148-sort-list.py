0e4e6b29ac160ce55f33723f6182b4aa7e2e1604
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-11 16:52:19 -0500

    Time: 217 ms (23.87%), Space: 31.9 MB (83.34%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,15 +5,42 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        curr = head
-        while curr:
-            val = curr.val
-            arr.append(val)
-            curr = curr.next
-        arr.sort()
-        curr = head
-        for i in arr:
-            curr.val = i
-            curr = curr.next
-        return head
+        if not head or not head.next:
+            return head
+
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left, right):
+        dummy = tail = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        while left:
+            tail.next = left
+            left = left.next
+            tail = tail.next
+        while right:
+            tail.next = right
+            right = right.next
+            tail = tail.next
+        return dummy.next
