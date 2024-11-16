75fcf941ff48f104c20905479be8e913f669cb8f
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-16 12:29:59 -0500

    Time: 3 ms (79.79%), Space: 16.6 MB (44.1%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,30 +1,27 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # Define a list of tuples with integer values and their corresponding Roman numeral symbols
-        val_map = [
-            (1000, 'M'),
-            (900, 'CM'),
-            (500, 'D'),
-            (400, 'CD'),
-            (100, 'C'),
-            (90, 'XC'),
-            (50, 'L'),
-            (40, 'XL'),
-            (10, 'X'),
-            (9, 'IX'),
-            (5, 'V'),
-            (4, 'IV'),
-            (1, 'I')
-        ]
+        hmap = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I",
+        }
 
-        # Initialize an empty result string to store the Roman numeral
-        result = ''
+        res = ""
+        for val , symb in hmap.items():
+            while num >= val:
+                num -= val
+                res += symb
+        return res
         
-        # Iterate through each tuple in val_map
-        for value, symbol in val_map:
-            # While num is greater than or equal to the current value, subtract the value and append the symbol
-            while num >= value:
-                result += symbol
-                num -= value
-        
-        return result
+
+
