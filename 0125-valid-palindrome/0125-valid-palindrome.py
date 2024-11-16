4ab1126179167c0c0be7d6020144a0e6f4d02bca
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-16 17:49:03 -0500

    Time: 11 ms (37.36%), Space: 17 MB (56.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        low = 0
+        high = n-1
+
+        while low < high :
+            #remove leading spaces
+            while low < high and not s[low].isalnum() :
+                low += 1
+            #removed tralining white spaces
+            while low < high and not s[high].isalnum():
+                high -= 1
+
+            if s[low].lower() != s[high].lower():
+                return False
+            
+            low += 1
+            high -= 1
+        
+        return True 
+            
+            
