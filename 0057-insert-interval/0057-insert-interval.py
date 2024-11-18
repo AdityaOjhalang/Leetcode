9ebffd24591fad7a3da6ca9c6c4c5e09166868ef
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-18 13:42:33 -0500

    Time: 7 ms (8.05%), Space: 18.7 MB (84.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -2,12 +2,17 @@ class Solution:
     def insert(
         self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        intervals.append(newInterval)
-        intervals.sort(key = lambda x : x[0])
-
-        for interval in intervals:
-            if not res or res[-1][1] < interval[0]:
+        for i , interval in enumerate(intervals):
+            #new interval is smaller than current 
+            if newInterval[1] < interval[0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            #new Interval is bigger than current then append current interval
+            elif interval[1] < newInterval[0]:
                 res.append(interval)
+            #They are overlapping intervals
             else:
-                res[-1][1] = max(res[-1][1],interval[1])
+                newInterval[0] = min(newInterval[0],interval[0])
+                newInterval[1] = max(newInterval[1],interval[1])
+        res.append(newInterval)
         return res
