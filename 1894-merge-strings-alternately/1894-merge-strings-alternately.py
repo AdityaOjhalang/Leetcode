2c7276b7c1000f8ecee1834d9b7f2910fea6ecc8
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-17 20:09:25 -0500

    Time: 31 ms (80.02%), Space: 16.6 MB (58.75%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,12 +5,18 @@ class Solution:
         i = 0
         j = 0
         res = []
-        while i < m or j < n:
-            if i < m:
-                res += word1[i]
-                i+=1
-            if j < n:
-                res += word2[j]
-                j+=1
+        while i < m and j < n:
+        
+            res += word1[i]
+            i+=1
+            
+            res += word2[j]
+            j+=1
+        while i < m:
+            res += word1[i]
+            i+=1
+        while j < n:
+            res += word2[j]
+            j+=1
         return "".join(res)
 
