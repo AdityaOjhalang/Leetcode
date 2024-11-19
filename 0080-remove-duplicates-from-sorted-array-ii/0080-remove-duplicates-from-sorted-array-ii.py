48db91e863cf5e1437e259d25dd1459013b52742
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-18 19:34:20 -0500

    Time: 54 ms (39.09%), Space: 16.5 MB (71.53%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,14 +3,11 @@ class Solution:
         toadd = 1
         count = 1
         for i in range(1,len(nums)):
-            if nums[i] == nums[i-1]:
-                count += 1
-            else:
+            if nums[i] != nums[i-1]:
                 count = 1
-            
+            else:
+                count +=1
             if count <= 2:
                 nums[toadd] = nums[i]
-                toadd+=1
-
+                toadd += 1
         return toadd
-
