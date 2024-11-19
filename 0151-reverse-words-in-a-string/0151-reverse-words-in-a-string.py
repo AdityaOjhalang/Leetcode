697ea7cbca8af47493fd7d8af27534b87f5a675d
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-18 20:49:37 -0500

    Time: 7 ms (9.48%), Space: 16.8 MB (43.36%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,30 +1,21 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         left, right = 0, len(s) - 1
-        # remove leading spaces
-        while left <= right and s[left] == " ":
-            left += 1
-
-        # remove trailing spaces
+        #removing spaces
+        while left <= right and s[left] == " ":              
+            left +=1
         while left <= right and s[right] == " ":
             right -= 1
-
-        # intialize word arr and deque
-        word , queue = [] , deque()
-
-        # if space is found
+        
+        word = []
+        queue = deque()
         while left <= right:
-            if s[left] == " " and word:
+            if word and s[left] == " ":
                 queue.appendleft("".join(word))
                 word = []
             elif s[left] != " ":
                 word.append(s[left])
             left += 1
-        
         queue.appendleft("".join(word))
         return " ".join(queue)
-
-
-        # no space is found
-
-        # return
+                
