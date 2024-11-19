0863267fb122622138b2f49485e9de25aea740f1
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-18 19:57:09 -0500

    Time: 3 ms (29.18%), Space: 17 MB (45.36%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -3,6 +3,6 @@ class Solution:
         citations.sort(reverse=True)
         hindex = 0
         for i in range(len(citations)):
-            if citations[i] <= i:
+            if citations[i] < i + 1:
                 return i
         return len(citations)
