9a7e3830d2adbcdd2b6d8015846c54118afe3044
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-18 19:53:15 -0500

    Time: 7 ms (82.08%), Space: 17.7 MB (21.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,13 +3,13 @@ class Solution:
         minjumps = 0
         farthest = 0
         currend = 0
+
         for i in range(len(nums)-1):
             farthest = max(farthest,nums[i]+i)
 
             if i == currend:
                 currend = farthest
                 minjumps += 1
-            
-            if currend >= len(nums) - 1:
+            if currend >= len(nums)-1:
                 break
         return minjumps
