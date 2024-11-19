f8f508c636e234c3787733c2a1956e83da7071d8
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-18 20:23:47 -0500

    Time: 0 ms (100%), Space: 16.6 MB (81.39%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -4,9 +4,9 @@ class Solution:
         count = 0
         for i in range(n-1,-1,-1):
             if count == 0 and s[i] == " ":
-                continue
+                 continue
             if count > 0 and s[i] == " ":
                 break
             else:
-                count +=1
+                count += 1
         return count
