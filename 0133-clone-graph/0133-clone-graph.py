dda16a75b7d383d1967f1b8815f50331b516ebd8
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-20 18:08:44 -0500

    Time: 43 ms (37.24%), Space: 17 MB (29.06%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -14,12 +14,12 @@ class Solution:
         def dfs(node):
             if node in oldnew:
                 return oldnew[node]
-            
+
             copy = Node(node.val)
             oldnew[node] = copy
             for neigh in node.neighbors:
                 copy.neighbors.append(dfs(neigh))
-            
             return copy
         
         return dfs(node) if node else None
+                
