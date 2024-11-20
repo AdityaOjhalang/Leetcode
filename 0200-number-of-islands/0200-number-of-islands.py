a3714b2079ff9276d559cbc2dde3167dc3504cd0
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-20 13:30:22 -0500

    Time: 259 ms (35.36%), Space: 27.3 MB (12.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #helper function to check whether the position is valid and is an island or not 
-        def valid(row, col):
-            return 0 <= row < rows and 0 <= col < cols and grid[row][col] == "1"
-        #recrusive DFS to find all neighbors in the set
-        def dfsNeigh(row, col):
-            for x, y in directions:
-                nextrow, nextcol = row + x, col + y
-                if valid(nextrow,nextcol) and (nextrow,nextcol) not in seen:
-                    seen.add((nextrow,nextcol))
-                    dfsNeigh(nextrow,nextcol)
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        seen = set()
-        #going in all directions to find neighbors
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         res = 0
-        #running the funciton
-        for i in range(rows):
-            for j in range(cols):
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        seen = set()
+
+        def valid(row,col):
+            return 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == "1"
+
+        def dfs(row,col):
+            for x,y in directions:
+                nr , nc = row + x , col + y
+                if valid(nr,nc) and (nr,nc) not in seen:
+                    seen.add((nr,nc))
+                    dfs(nr,nc)
+
+        for i in range(ROWS):
+            for j in range(COLS):
                 if grid[i][j] == "1" and (i,j) not in seen:
                     seen.add((i,j))
-                    res +=1
-                    dfsNeigh(i,j)
-        return res
+                    res += 1
+                    dfs(i,j)
+        return res 
+
+
+
