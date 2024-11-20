2818fed18e75f79a2c2ee59b7d75eb4e1dc62aa5
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-20 16:27:25 -0500

    Time: 5 ms (71.93%), Space: 20.8 MB (34.76%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,39 +5,36 @@ class Solution:
         """
         if not board:
             return
-        m,n = len(board),len(board[0])
-        visited = set()
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
 
-        def dfs(row, col):
-            if (row < 0 or row >= m or col < 0 or col >= n) or board[row][col] != "O":
+        ROWS, COLS = len(board), len(board[0])
+        vistied = set()
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        def mark(r, c):
+            if (r < 0 or r >= ROWS or c < 0 or c >= COLS) or board[r][c] != "O":
                 return
-            board[row][col] = "E"  # Mark as visited
+            board[r][c] = "E"
+            vistied.add((r,c))
             for x, y in directions:
-                nr, nc = row + x, col + y
-                dfs(nr, nc)
+                nr, nc = x + r, y + c
+                mark(nr,nc)
 
-
-        for r in range(m):
+        for r in range(ROWS):
             if board[r][0] == "O":
-                dfs(r,0)
-            if board[r][n-1] == "O":
-                dfs(r,n-1)
-        
-        for c in range(n):
-            if board[0][c] == "O":
-                dfs(0,c)
-            if board[m-1][c] == "O":
-                dfs(m-1,c)
+                mark(r,0)
+            if board[r][COLS-1] == "O":
+                mark(r,COLS-1)
 
-        for i in range(m):
-            for j in range(n):
+        for c in range(COLS):
+            if board[0][c] == "O":
+                mark(0,c)
+            if board[ROWS-1][c] == "O":
+                mark(ROWS-1,c)
+        
+        for i in range(ROWS):
+            for j in range(COLS):
                 if board[i][j] == "O":
                     board[i][j] = "X"
-        
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == "E":
-                    board[i][j] = "O"        
-
+                elif board[i][j] == "E":
+                    board[i][j] = "O"
         
