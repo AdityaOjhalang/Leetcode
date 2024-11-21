2c3d4625282a0a53bed82fc0a5694f6f81a93656
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-21 14:02:07 -0500

    Time: 516 ms (14.24%), Space: 24.6 MB (32.84%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,31 +1,24 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        def valid(row, col):
-            return 0 <= row < m and 0 <= col < n
-
-        m = len(grid)
-        n = len(grid[0])
-        queue = deque([(0, 0, k, 0)])
-        seen = {(0, 0, k)}
-
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        def valid(r,c):
+            return 0 <= r < ROWS and 0 <= c < COLS #not doing element check cause we can remove obstacles
+        
+        ROWS , COLS = len(grid) , len(grid[0])
+        seen = {(0,0,k)}
+        queue = deque([(0,0,k,0)])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
 
         while queue:
-            row, col, rem, steps = queue.popleft()
-
-            if row == m-1 and col == n-1:
+            row,col,obs,steps = queue.popleft()
+            if row == ROWS - 1 and col == COLS -1:
                 return steps
-
             for x, y in directions:
-                nextr, nextc = x + row, y + col
-
-                if valid(nextr, nextc):
-
-                    if (nextr, nextc, rem) not in seen and grid[nextr][nextc] == 0:
-                        seen.add((nextr, nextc,rem))
-                        queue.append((nextr, nextc, rem, steps + 1))
-                    
-                    elif (nextr,nextc, rem -1 ) not in seen and rem:
-                        seen.add((nextr, nextc, rem-1))
-                        queue.append((nextr, nextc, rem-1, steps + 1))
+                nr , nc = row + x, col + y
+                if valid(nr,nc):
+                    if (nr,nc,obs) not in seen and grid[nr][nc] == 0:
+                        seen.add((nr,nc,obs))
+                        queue.append((nr,nc,obs,steps+1))
+                    elif (nr,nc,obs-1) not in seen and grid[nr][nc] == 1 and obs:
+                        seen.add((nr,nc,obs-1))
+                        queue.append((nr,nc,obs-1,steps+1))
         return -1
