097d6def3f6ac447d84c1a12dde8d4d3219cb3bc
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-20 19:52:43 -0500

    Time: 0 ms (100%), Space: 16.9 MB (18.13%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,35 +1,37 @@
 class Solution:
-    def calcEquation(
-        self, equations: List[List[str]], values: List[float], queries: List[List[str]]
-    ) -> List[float]:
-
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         neighbors = defaultdict(list)
 
         for i in range(len(equations)):
-            x, y = equations[i]
+            x , y = equations[i]
             val = values[i]
-            neighbors[x].append((y, val))
-            neighbors[y].append((x, 1 / val))
+            neighbors[x].append((y,val))
+            neighbors[y].append((x,1/val))
+        
+        
 
-        def querycalc(start, end):
+        def querycalc(start,end):
             if start not in neighbors or end not in neighbors:
                 return -1
-
-            queue = deque([(start, 1)])
+            
             seen = {start}
+            queue = deque([(start,1)])
 
             while queue:
-                node, ratio = queue.popleft()
+                node,ratio = queue.popleft()
                 if node == end:
                     return ratio
-                for neigh, weight in neighbors[node]:
+                for neigh,weight in neighbors[node]:
                     if neigh not in seen:
                         seen.add(neigh)
-                        queue.append((neigh, ratio * weight))
-            
+                        queue.append((neigh,ratio*weight))
             return -1
-        ans = []        
+        
+        ans = []
         for i in range(len(queries)):
-            start,end = queries[i]
+            start, end = queries[i]
             ans.append(querycalc(start,end))
         return ans
+            
+
+        
