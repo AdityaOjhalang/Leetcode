5ecdc98179aacd9f9ead8961e11303b9fb1b751a
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-21 17:56:43 -0500

    Time: 12 ms (92.55%), Space: 16.8 MB (31.5%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,7 +1,7 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        cells = [None] * ( n**2 + 1)
+        cells = [None] * ( n**2 + 1 )
         columns = list(range(n))
         label = 1
         for row in range(n-1,-1,-1):
@@ -11,19 +11,19 @@ class Solution:
             columns.reverse()
         
         queue = deque([(1,0)])
-        seen = {1}
+        visited = {1}
 
         while queue:
             square , moves = queue.popleft()
-
             for i in range(1,7):
-                nextsq = square+i
+                nextsq = square + i
                 row,col = cells[nextsq]
                 if board[row][col] != -1:
                     nextsq = board[row][col]
-                if nextsq == n*n:
+                if nextsq == n * n:
                     return moves + 1
-                if nextsq not in seen:
-                    seen.add(nextsq)
+                if nextsq not in visited:
+                    visited.add(nextsq)
                     queue.append((nextsq,moves+1))
         return -1
+
