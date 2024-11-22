997b3362d1dc3827bc03eb7c9521aa1d9f754c7b
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-22 18:25:37 -0500

    Time: 561 ms (52.68%), Space: 28.3 MB (92.6%) - LeetHub

--- 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.py
@@ -3,13 +3,10 @@ class Solution:
         heap = [-num for num in piles]
         heapq.heapify(heap)
 
-        ans = 0
-
-        while ans < k:
+        for i in range(k):
             val = -heapq.heappop(heap)
             toremove = val//2
             heapq.heappush(heap,-(val-toremove))
-            ans+=1
-
+            
         return -sum(heap)
         
