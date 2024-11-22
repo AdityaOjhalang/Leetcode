e0023afb7810f55bfb67ae3e20a941f6445a5cc7
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-22 18:11:45 -0500

    Time: 0 ms (100%), Space: 16.5 MB (89.58%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -8,6 +8,6 @@ class Solution:
             second = abs(heapq.heappop(stones))
 
             if first != second:
-                heapq.heappush(stones,-abs(first-second))
+                heapq.heappush(stones,-(first-second))
         
         return -(stones[0]) if stones else 0
