cb4cfc4992f01946efa371163e5423b99fb204cb
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-21 19:20:18 -0500

    Time: 19 ms (17.31%), Space: 22.2 MB (91.17%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.py
+++ 1428-jump-game-iii/1428-jump-game-iii.py
@@ -1,39 +1,31 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-
         n = len(arr)
-        
+        def valid(ind):
+            return 0 <= ind < n
         def neighbors(ind):
-
-            ans = []
+            res = []
             first = ind + arr[ind]
             second = ind - arr[ind]
-            
-            if 0 <= first < n:
-                ans.append(first)
-            if 0 <= second < n:
-                ans.append(second)
 
-            return ans 
+            if valid(first):
+                res.append(first)
+            if valid(second):
+                res.append(second)
+            return res
         
         if arr[start] == 0:
             return True
-            
+        
         queue = deque([(start)])
-        seen = set()
-        seen.add(start)
+        seen = {start}
 
         while queue:
-            nodeinx = queue.popleft()
-            if arr[nodeinx] == 0:
+            ind = queue.popleft()
+            if arr[ind] == 0:
                 return True
-            
-            for neigh in neighbors(nodeinx):
+            for neigh in neighbors(ind):
                 if neigh not in seen:
                     seen.add(neigh)
                     queue.append(neigh)
-        
         return False
-
-        
-
