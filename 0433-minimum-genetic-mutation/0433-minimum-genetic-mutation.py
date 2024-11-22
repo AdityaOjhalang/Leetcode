c757a6cb55590021a5500b831ab449e6a33a1dc3
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-21 19:03:54 -0500

    Time: 0 ms (100%), Space: 16.5 MB (78.78%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,29 +1,28 @@
 class Solution:
-    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
         
-        def mutations(node):
-            arr = []
-
+        def neighbors(node):
+            res = []
             for i in range(len(node)):
                 for char in "ACGT":
-                    if char != node[i]:
-                        arr.append(node[:i] + char + node[i+1:])    
-            return arr
+                    if node[i] != char:
+                        res.append(node[:i] + char + node[i+1:])
+            return res
+            
+
         
-        queue = deque([(startGene,0)])
-        seen = {startGene}
+        seen = {start}
         bank = set(bank)
+        queue = deque([(start,0)])
 
         while queue:
             node,steps = queue.popleft()
-
-            if node == endGene:
-                return steps 
-
-            for neigh in mutations(node):
-                if neigh not in seen:
+            if node == end:
+                return steps
+            for neigh in neighbors(node):
+                if neigh not in seen and neigh in bank:
                     seen.add(neigh)
+                    queue.append((neigh,steps+1))
+        return -1
 
-                    if neigh in bank:
-                        queue.append((neigh,steps+1))
-        return -1 
+
