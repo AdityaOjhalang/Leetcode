d3862a00659c18ba84cbbc77df3a85587c1d74d5
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-21 20:12:18 -0500

    Time: 330 ms (61.43%), Space: 17 MB (70.42%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
@@ -1,34 +1,32 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        graph = defaultdict(list)
         n = len(bombs)
-
+        graph = defaultdict(list)
         for i in range(n):
             for j in range(n):
                 if i == j:
                     continue
-                xi, yi, ri = bombs[i]
-                xj, yj, rj = bombs[j]
+                xi , yi , ri = bombs[i]
+                xj , yj , rj = bombs[j]
 
-                if (ri**2) >= ((xi - xj) ** 2 + (yi - yj) ** 2):
+                if ri ** 2 >= (xi - xj)**2 + (yi - yj)**2 :
                     graph[i].append(j)
         
-        def bfs(i):
-            queue = deque([i])
+        def connected(bomb):
+            queue = deque([(bomb)])
             seen = set([i])
             while queue:
                 bomb = queue.popleft()
                 for neigh in graph[bomb]:
                     if neigh not in seen:
                         seen.add(neigh)
-                        queue.append(neigh)
+                        queue.append((neigh))
             return len(seen)
-
+        
         ans = 0
-
         for i in range(n):
-            ans = max(ans,bfs(i))
-        
-        return ans
-        
+            ans = max(ans,connected(i))
+        return ans 
+
+
 
