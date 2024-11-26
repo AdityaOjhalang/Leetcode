38ac3db34fcf2d0edbbd5a5a0f9231a97ca3ad64
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-26 17:55:46 -0500

    Time: 136 ms (15.26%), Space: 20 MB (14.2%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -1,27 +1,22 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         def valid(r,c):
-            return 0 <= r < ROWS and 0 <= c < COLS and maze[r][c] == "."
+            return 0<=r<m and 0<=c<n and maze[r][c] == "." and (r,c) not in seen
         def validExit(r,c):
-            return (r in {0,ROWS-1} or c in {0,COLS-1}) and (r,c) != (x,y) and maze[r][c] == "."
+            return ( r in {0,m-1} or  c in {0,n-1}) and (r,c) != (x_start,x_end) 
         
-        ROWS , COLS = len(maze) , len(maze[0])
-        x,y = entrance
-        queue = deque([(x,y,0)])
-        seen = {(x,y)}
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
-
+        m,n = len(maze) , len(maze[0])
+        x_start , x_end = entrance
+        queue = deque([(x_start,x_end,0)])
+        seen = {(x_start,x_end)}
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         while queue:
             row, col, steps = queue.popleft()
             if validExit(row,col):
                 return steps
             for x,y in directions:
                 nr,nc = row+x,col+y
-                if valid(nr,nc) and (nr,nc) not in seen:
+                if valid(nr,nc):
                     seen.add((nr,nc))
                     queue.append((nr,nc,steps+1))
         return -1
-
-
-        
-
