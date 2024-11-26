be98ed5135285ab8ab9d2dc9a15c738015af4ee6
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-26 13:25:35 -0500

    Time: 0 ms (100%), Space: 17.3 MB (5.99%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,37 +1,31 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        neighbors = defaultdict(list)
+        graph = defaultdict(list)
 
         for i in range(len(equations)):
             x , y = equations[i]
             val = values[i]
-            neighbors[x].append((y,val))
-            neighbors[y].append((x,1/val))
+            graph[x].append((y,val))
+            graph[y].append((x,1/val))
         
-        
-
-        def querycalc(start,end):
-            if start not in neighbors or end not in neighbors:
+        def dfs(start,end):
+            if start not in graph or end not in graph:
                 return -1
             
-            seen = {start}
             queue = deque([(start,1)])
+            seen = {start}
 
             while queue:
                 node,ratio = queue.popleft()
                 if node == end:
                     return ratio
-                for neigh,weight in neighbors[node]:
+                
+                for neigh,weight in graph[node]:
                     if neigh not in seen:
                         seen.add(neigh)
-                        queue.append((neigh,ratio*weight))
+                        queue.append((neigh,weight*ratio))
             return -1
-        
-        ans = []
-        for i in range(len(queries)):
-            start, end = queries[i]
-            ans.append(querycalc(start,end))
-        return ans
-            
-
-        
+        res = []
+        for x,y in queries:
+            res.append(dfs(x,y))
+        return res
