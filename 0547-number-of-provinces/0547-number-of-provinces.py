59c40e968951e337c3facee20a6819aa38582c11
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-26 12:53:20 -0500

    Time: 8 ms (37.53%), Space: 18.8 MB (6.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -7,20 +7,23 @@ class Solution:
         res = 0
 
         for i in range(n):
-            for j in range(i+1,n):
-                if isConnected[i][j] == 1:
+            for j in range(n):
+                if i == j:
+                    continue
+                elif isConnected[i][j] == 1:
                     graph[i].append(j)
                     graph[j].append(i)
-        
+
         def dfs(node):
             for neigh in graph[node]:
                 if neigh not in seen:
                     seen.add(neigh)
                     dfs(neigh)
-        
+
         for i in range(n):
             if i not in seen:
-                seen.add(i)
                 res += 1
+                seen.add(i)
                 dfs(i)
-        return res 
+
+        return res
