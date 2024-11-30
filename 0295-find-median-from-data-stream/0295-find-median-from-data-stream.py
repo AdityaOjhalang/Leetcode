c337d24dc6031a62c0f0f66c566f96f154949593
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-11-30 15:08:34 -0500

    Time: 109 ms (88.55%), Space: 39.4 MB (18.44%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,34 +1,32 @@
-import heapq
-
 class MedianFinder:
 
     def __init__(self):
-        self.small, self.large = [], []  
+        self.small , self.large = [],[]
+
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -1 * num)
+        heapq.heappush(self.small,-num)
 
-        # Ensure the max element of small is not greater than the min element of large
-        if self.small and self.large and (-self.small[0] > self.large[0]):
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-
-        # Balance the sizes of the two heaps if necessary
+        if self.small and self.large and -self.small[0] > self.large[0]:
+            val = - heapq.heappop(self.small)
+            heapq.heappush(self.large,val)
+        
+        #this migh imbalance so balancing
         if len(self.small) > len(self.large) + 1:
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-        elif len(self.large) > len(self.small):
-            val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1 * val)
+            val = - heapq.heappop(self.small)
+            heapq.heappush(self.large,val)
+        if len(self.large) > len(self.small) + 1:
+            val = - heapq.heappop(self.large)
+            heapq.heappush(self.small,val)
 
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
-            return -self.small[0]  # Convert back to positive
+            return -self.small[0]
         if len(self.large) > len(self.small):
             return self.large[0]
-        return (-self.small[0] + self.large[0]) / 2
+        return (-self.small[0] + self.large[0]) / 2 
 
 
-# Example usage:
+# Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
