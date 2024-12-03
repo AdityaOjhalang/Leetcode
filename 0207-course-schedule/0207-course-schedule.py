ab5c6c044e5448da85ec9f9ef95e6d3c5e3b7e25
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-12-03 10:36:43 -0500

    Time: 4 ms (69.43%), Space: 18.8 MB (9.38%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,14 +1,12 @@
 class Solution:
     def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
-        
         graph = defaultdict(list)
 
-        for crs, p in prerequisites:
-            graph[crs].append(p)
+        for crs,preq in prerequisites:
+            graph[crs].append(preq)
         
-        UNVISTITED , VISITING , VISITED = 0,1,2
-
-        state = [UNVISTITED] * n
+        state = [0] * n
+        UNVISITED,VISITING,VISITED = 0,1,2
 
         def dfs(crs):
             if state[crs] == VISITING:
@@ -21,13 +19,10 @@ class Solution:
                 if not dfs(preq):
                     return False
             
-            state[crs] = VISITED
+            state[crs] = VISITED 
             return True
         
         for i in range(n):
             if not dfs(i):
                 return False
-        return True
-
-        
-        
+        return True 
