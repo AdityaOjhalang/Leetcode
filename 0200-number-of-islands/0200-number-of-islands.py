53ae90b6c3e59a9310cce4934893ae7691465611
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-12-03 10:31:44 -0500

    Time: 296 ms (15.68%), Space: 27.9 MB (9.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
-        ROWS = len(grid)
-        COLS = len(grid[0])
+        ROWS, COLS = len(grid), len(grid[0])
         seen = set()
-        res = 0
-
-        def valid(row,col):
-            return 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == "1" and (row,col) not in seen
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         
-        def dfs(row,col):
-            for x,y in directions:
-                nr,nc = row+x,col+y
+        def valid(r,c):
+            return 0<=r<ROWS and 0<=c<COLS and grid[r][c] == "1" and (r,c) not in seen
+
+        def dfs(row, col):
+            for x, y in directions:
+                nr, nc = row + x, col + y
                 if valid(nr,nc):
                     seen.add((nr,nc))
                     dfs(nr,nc)
-        
+
+        islands = 0
         for i in range(ROWS):
             for j in range(COLS):
                 if valid(i,j):
-                    res+=1
                     seen.add((i,j))
+                    islands += 1
                     dfs(i,j)
-        return res
+
+        return islands
+            
