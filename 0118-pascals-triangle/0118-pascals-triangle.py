c3b414c2923b761675c735a1ab65e3a66dfcb640
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-12-05 11:49:08 -0500

    Time: 0 ms (100%), Space: 17.4 MB (7.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,7 +3,7 @@ class Solution:
         triangle = []
         for row_num in range(numRows):
             row = [None for _ in range(row_num+1)]
-            row[0],row[-1] = 1,1
+            row[0]=row[-1] = 1
 
             for j in range(1,len(row)-1):
                 row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
