c1a7d0424fa202602e676bc0a4c24bf92f5950f3
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2024-12-06 12:50:53 -0500

    Time: 0 ms (100%), Space: 17.1 MB (13.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,20 +1,12 @@
 class Solution:
-    def binaryExp(self, x: float, n: int) -> float:
-        # Base case, to stop recursive calls.
-        if n == 0:
-            return 1
-
-        # Handle case where, n < 0.
-        if n < 0:
-            return 1.0 / self.binaryExp(x, -1 * n)
-
-        # Perform Binary Exponentiation.
-        # If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
-        if n % 2 == 1:
-            return x * self.binaryExp(x * x, (n - 1) // 2)
-        # Otherwise we calculate result by performing Binary Exponentiation on 'n'.
-        else:
-            return self.binaryExp(x * x, n // 2)
-
     def myPow(self, x: float, n: int) -> float:
-        return self.binaryExp(x, n)
+        def power(x,n):
+            if x == 0: return 0
+            if n == 0: return 1
+
+            res = power(x,n//2)
+            res = res * res 
+            return res * x if n % 2 == 1 else res 
+        
+        res = power(x,abs(n))
+        return res if n >= 0 else 1/res
