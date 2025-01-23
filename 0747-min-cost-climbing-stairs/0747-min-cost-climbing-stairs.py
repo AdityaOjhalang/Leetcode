38763d0d392a97251c1d372599468e4e71d8c2eb
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-01-23 14:42:53 -0500

    Time: 7 ms (22.48%), Space: 20.1 MB (6.08%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,18 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
         def dp(i):
-            #base case
             if i == 0 or i == 1:
                 return 0
-
-            #check memo
             if i in memo:
                 return memo[i]
-
-            #recurrence relation
-            memo[i] = min(dp(i-1) + cost[i-1],dp(i-2) + cost[i-2])
+            
+            memo[i] = min(dp(i-1)+cost[i-1], dp(i-2)+cost[i-2])
             return memo[i]
-
         memo = {}
         return dp(len(cost))
