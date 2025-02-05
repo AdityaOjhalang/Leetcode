3294c3383eaf5dec909f612cfec865df3ae7af8f
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-02-05 15:07:02 -0500

    Time: 39 ms (87.28%), Space: 20.1 MB (70.78%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        @cache
+        def dp(i,j):
+            if i == len(word1): return len(word2) - j
+            if j == len(word2): return len(word1) - i
+        
+            # if the letters match
+            if word1[i] == word2[j] :
+                return dp(i+1,j+1)
+            
+            return 1 + min(
+                dp(i,j+1), #insert
+                dp(i+1,j), #delete
+                dp(i+1,j+1)
+            )
+        
+        return dp(0,0)
