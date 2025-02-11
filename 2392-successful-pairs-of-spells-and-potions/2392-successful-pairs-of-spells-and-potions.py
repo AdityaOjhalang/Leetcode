48e63f5e8b39c06912baae23781c099839aedf15
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-02-10 20:15:33 -0500

    Time: 491 ms (59.69%), Space: 39.3 MB (41.85%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -1,25 +1,20 @@
 class Solution:
-    def successfulPairs(
-        self, spells: List[int], potions: List[int], success: int
-    ) -> List[int]:
-        def leftstart(arr, target):
-            left = 0
-            right = len(arr)
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        res = []
 
+        def leftindx(arr,target):
+            left , right = 0 , len(arr)
             while left < right:
                 mid = (left + right) // 2
-                val = arr[mid]
-                if val >= target:
+                if arr[mid] >= target:
                     right = mid 
                 else:
                     left = mid + 1
-
             return left
-
-        potions.sort()
-        res = []
+        
         n = len(potions)
         for spell in spells:
-            ind = leftstart(potions, success / spell)
-            res.append(n - ind)
-        return res
+            ind = leftindx(potions,success/spell)
+            res.append(n-ind)
+        return res 
