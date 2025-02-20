e9c8e726f9721db36adbdde0c031af7460c4b54e
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-02-20 12:13:56 -0500

    Time: 0 ms (100%), Space: 18 MB (25.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #taking 3 pointers , pointing one at end of first one , secont at second one and third at m+n -1 poisition
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
+        i, j, k = m - 1, n - 1, m + n - 1
+
         while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
+            if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
-            else :
+            else:
                 nums1[k] = nums2[j]
-                j-=1
+                j -= 1
             k -= 1
-        while j >= 0:
+        
+        while j >= 0 :
             nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+            j-=1
+            k-=1
 
