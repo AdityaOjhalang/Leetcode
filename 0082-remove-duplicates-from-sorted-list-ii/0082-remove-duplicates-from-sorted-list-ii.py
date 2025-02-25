096854628c787aebbd37546d8023daeb8025e829
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-02-25 11:09:29 -0500

    Time: 0 ms (100%), Space: 17.9 MB (43.08%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,16 +5,19 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #remove all values that repeast includingoriginga
         dummy = ListNode(0,head)
         prev = dummy
+        
         while head:
             if head.next and head.next.val == head.val:
-                while head.next and head.val == head.next.val :
+                while head.next and head.next.val == head.val:
                     head = head.next
                 #purge
-                prev.next = head.next
+                prev.next = head.next 
             else:
                 prev = prev.next
             head = head.next
+        
         return dummy.next
+
+
