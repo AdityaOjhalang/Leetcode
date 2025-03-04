d9fdcf98ddbe1c5deba5a11a1c69b4dcd6130174
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-04 13:16:41 -0500

    Time: 7 ms (65.62%), Space: 25.1 MB (19.21%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -8,20 +8,17 @@ class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
         self.stack = []
-        curr = root
-        while curr:
-            self.stack.append(curr)
-            curr = curr.left
+        while root:
+            self.stack.append(root)
+            root = root.left
 
     def next(self) -> int:
         node = self.stack.pop()
-        curr = node.right
+        curr = node.right if node.right else None 
         while curr:
             self.stack.append(curr)
             curr = curr.left
         return node.val
-
-        
 
     def hasNext(self) -> bool:
         return len(self.stack) != 0
