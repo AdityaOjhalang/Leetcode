16d37cd06aba3687602e57b037547feef88cd0de
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-05 13:37:21 -0500

    Time: 184 ms (23.83%), Space: 74.5 MB (42.15%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.py
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.py
@@ -1,19 +1,15 @@
 class Solution:
     def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
-
-        def dfs(node):
-            seen.add(node)
-            for neigh in graph[node]:
-                if neigh not in seen and neigh not in restr:
-                    seen.add(neigh)
-                    dfs(neigh)
-
+        seen = {0}
+        restr = set(restricted)
         graph = defaultdict(list)
         for x,y in edges:
             graph[x].append(y)
             graph[y].append(x)
-        
-        restr = set(restricted)
-        seen = set()
+        def dfs(node):
+            for neigh in graph[node]:
+                if neigh not in seen and neigh not in restr:
+                    seen.add(neigh)
+                    dfs(neigh)
         dfs(0)
         return len(seen)
