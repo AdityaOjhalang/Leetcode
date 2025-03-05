2ff25e3641e97204f11dc007595dd66db7ac7034
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-05 13:33:04 -0500

    Time: 29 ms (37.83%), Space: 19.6 MB (16.73%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,30 +1,25 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        ROWS,COLS = len(grid),len(grid[0])
         seen = set()
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-
-        def valid(row, col):
-            return (
-                0 <= row < ROWS
-                and 0 <= col < COLS
-                and grid[row][col] == 1
-                and (row, col) not in seen
-            )
-
-        def dfs(row, col):
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        def valid(r,c):
+            return 0 <= r < ROWS and 0 <= c < COLS
+        def dfs(row,col):
             area = 1
-            for x, y in directions:
-                nr, nc = row + x, col + y
-                if valid(nr, nc):
-                    seen.add((nr, nc))
-                    area += dfs(nr, nc)
+            for x,y in directions:
+                nr,nc = row+x,col+y
+                if valid(nr,nc) and (nr,nc) not in seen and grid[nr][nc] == 1:
+                    seen.add((nr,nc))
+                    area += dfs(nr,nc)
             return area
-
-        res = 0
+        maxarea = 0
         for i in range(ROWS):
             for j in range(COLS):
-                if valid(i,j):
-                    seen.add((i, j))
-                    res = max(res, dfs(i, j))
-        return res
+                if grid[i][j] == 1 and (i,j) not in seen:
+                    seen.add((i,j))
+                    area = dfs(i, j)
+                    maxarea = max(area, maxarea)
+        return maxarea
+
+
