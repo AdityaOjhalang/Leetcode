439caf352cd62afd1df31a82c0292c5c330eb694
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-05 13:11:59 -0500

    Time: 2 ms (93.56%), Space: 19.7 MB (17.32%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,22 +1,23 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        seen = set()
+
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+        
         def dfs(node):
-            seen.add(node)
             for neigh in graph[node]:
                 if neigh not in seen:
                     seen.add(neigh)
                     dfs(neigh)
 
-        graph = defaultdict(list)
-        for x,y in edges:
-            graph[x].append(y)
-            graph[y].append(x)
-
-        seen = set()
-        res = 0
+        components = 0
         for i in range(n):
             if i not in seen:
                 seen.add(i)
+                components += 1
                 dfs(i)
-                res += 1 
-        return res
+
+        return components 
