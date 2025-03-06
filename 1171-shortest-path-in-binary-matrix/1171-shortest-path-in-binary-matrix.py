0a740addf506c7efaec149e9ec23775d4772ea48
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-06 12:10:05 -0500

    Time: 295 ms (20.94%), Space: 19.8 MB (7.3%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,27 +1,25 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        def valid(row,col):
-            return 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] != 1
-        
-        ROWS = len(grid)
-        COLS = len(grid[0])
-
-
         if grid[0][0] == 1:
-            return -1
+            return - 1
         
+        ROWS,COLS = len(grid),len(grid[0])
         directions = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
-        queue = deque([(0,0,1)])
         seen = {(0,0)}
-
+        queue = deque([(0,0,1)])
+        n = len(grid)
+        
+        def valid(r,c):
+            return 0<=r<ROWS and 0<=c<COLS and grid[r][c] == 0 and (r,c) not in seen
+        
         while queue:
             row,col,steps = queue.popleft()
-            if (row,col) == (ROWS-1,COLS-1):
+            if (row,col) == (n-1,n-1):
                 return steps
             for x,y in directions:
-                nr , nc = row + x , col + y
-                if valid(nr,nc) and (nr,nc) not in seen:
+                nr,nc = row+x,col+y
+                if valid(nr,nc):
                     seen.add((nr,nc))
                     queue.append((nr,nc,steps+1))
+        
         return -1
-
