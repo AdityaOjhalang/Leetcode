332a73f7cb34de407eba374f4b99ecca94fea577
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-10 15:28:11 -0400

    Time: 299 ms (19.69%), Space: 19.5 MB (44.65%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,25 +1,24 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1:
-            return - 1
+            return -1
         
         ROWS,COLS = len(grid),len(grid[0])
-        directions = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
         seen = {(0,0)}
+        directions = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
         queue = deque([(0,0,1)])
         n = len(grid)
-        
+
         def valid(r,c):
             return 0<=r<ROWS and 0<=c<COLS and grid[r][c] == 0 and (r,c) not in seen
         
         while queue:
             row,col,steps = queue.popleft()
-            if (row,col) == (n-1,n-1):
+            if (row,col) == (ROWS-1,COLS-1):
                 return steps
             for x,y in directions:
                 nr,nc = row+x,col+y
                 if valid(nr,nc):
                     seen.add((nr,nc))
                     queue.append((nr,nc,steps+1))
-        
         return -1
