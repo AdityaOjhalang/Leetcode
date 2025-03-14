91535463c5f9ecc9f267828fd7a23dc492367454
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-14 16:09:36 -0400

    Time: 341 ms (61.59%), Space: 18.2 MB (59%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
@@ -2,29 +2,28 @@ class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         graph = defaultdict(list)
         n = len(bombs)
-
         for i in range(n):
             for j in range(n):
-                if i == j:
+                if i == j :
                     continue
                 
-                xi, yi, ri = bombs[i]
-                xj, yj, rj = bombs[j]
+                xi,yi,ri = bombs[i]
+                xj,yj,rj = bombs[j]
 
-                if ri ** 2 >= (xi - xj)**2 + (yi - yj)**2:
+                if ri ** 2 >= (xi-xj) ** 2 + (yi-yj) ** 2:
                     graph[i].append(j)
         
-        def connected(bomb):
-            seen = {bomb}
-            queue = deque([(bomb)])
-            while queue:
-                bomb = queue.popleft()
-                for neigh in graph[bomb]:
-                    if neigh not in seen:
-                        seen.add(neigh)
-                        queue.append((neigh))
+        def connected(node,seen):
+            seen.add(node)
+            for neigh in graph[node]:
+                if neigh not in seen:
+                    seen.add(neigh)
+                    connected(neigh,seen)
             return len(seen)
+            
         res = 0
         for i in range(n):
-            res = max(res,connected(i))
-        return res
+            seen = set()
+            res = max(res,connected(i,seen))
+        return res 
+
