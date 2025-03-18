b82c57f1dcf2a0b104f9c89339f3789ff10bcf44
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-18 15:12:50 -0400

    Time: 7 ms (59.15%), Space: 21.7 MB (73.9%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,32 +3,31 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board:
-            return
+        ROWS,COLS = len(board),len(board[0])
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        def valid(r,c):
+            return 0<=r<ROWS and 0<=c<COLS and board[r][c] == "O"
 
-        ROWS, COLS = len(board), len(board[0])
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        def dfs(row, col):
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS or board[row][col] != "O":
-                return
-
+        def edgedfs(row,col):
+            if not valid(row,col):
+                return 
             board[row][col] = "E"
-            for x, y in directions:
-                nr, nc = row + x, col + y
-                dfs(nr, nc)
+            for x,y in directions:
+                nr,nc = row+x,col+y
+                if valid(nr,nc):
+                    edgedfs(nr,nc)
 
-        for c in range(COLS):
-            if board[0][c] == "O":
-                dfs(0,c)
-            if board[ROWS-1][c] == "O":
-                dfs(ROWS-1,c)
-        
-        for r in range(ROWS):
-            if board[r][0] == "O":
-                dfs(r,0)
-            if board[r][COLS-1] == "O":
-                dfs(r,COLS-1)
+        for col in range(COLS):
+            if board[0][col] == "O":
+                edgedfs(0,col)
+            if board[ROWS-1][col] == "O":
+                edgedfs(ROWS-1,col)
+        for row in range(ROWS):
+            if board[row][0] == "O":
+                edgedfs(row,0)
+            if board[row][COLS-1] == "O":
+                edgedfs(row,COLS-1)
         
         for i in range(ROWS):
             for j in range(COLS):
@@ -36,3 +35,5 @@ class Solution:
                     board[i][j] = "X"
                 if board[i][j] == "E":
                     board[i][j] = "O"
+                
+
