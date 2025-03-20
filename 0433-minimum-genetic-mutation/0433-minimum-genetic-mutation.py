a6f028a658e8ccc112672a34b692b312b639728a
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-20 17:55:41 -0400

    Time: 0 ms (100%), Space: 17.6 MB (90.58%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -4,20 +4,21 @@ class Solution:
             res = []
             for i in range(len(node)):
                 for char in "ACGT":
-                    if char != node[i]:
+                    if char != node:
                         res.append(node[0:i] + char + node[i+1:])
             return res
         
-        seen = {startGene}
-        bank = set(bank)
         queue = deque([(startGene,0)])
+        bank = set(bank)
+        seen = {startGene}
 
         while queue:
-            node, steps = queue.popleft()
+            node,steps = queue.popleft()
             if node == endGene:
                 return steps
             for neigh in neighbors(node):
                 if neigh not in seen and neigh in bank:
                     seen.add(neigh)
                     queue.append((neigh,steps+1))
+        
         return -1
