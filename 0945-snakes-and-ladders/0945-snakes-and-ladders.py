7bf1d3ddfcf265e793dc2ed34d3f5c2b50faaffb
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-20 16:21:19 -0400

    Time: 15 ms (96.28%), Space: 18.2 MB (18.16%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,32 +1,32 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        cells = [None] * (n**2 + 1)
+        cells = [None] * (n**2+1)
         label = 1
         columns = list(range(n))
-        for i in range(n-1,-1,-1):
-            for j in columns:
-                cells[label] = (i,j)
+        for row in range(n-1,-1,-1):
+            for col in columns:
+                cells[label] = (row,col)
                 label += 1
             columns.reverse()
-        
-        visited = {1}
+
         queue = deque([(1,0)])
+        seen = {1}
 
         while queue:
-            square,rolls = queue.popleft()
-            if cells[square] == n*n:
-                return rolls 
+            node,steps = queue.popleft()
+            if node == n**2:
+                return steps
             for i in range(1,7):
-                nextsq = square + i
+                nextsq = node + i
                 row,col = cells[nextsq]
                 if board[row][col] != -1:
                     nextsq = board[row][col]
-                if nextsq == n * n:
-                    return rolls + 1
-                if nextsq not in visited:
-                    visited.add(nextsq)
-                    queue.append((nextsq,rolls+1))
+                if nextsq == n**2:
+                    return steps + 1
+                if nextsq not in seen:
+                    seen.add(nextsq)
+                    queue.append((nextsq,steps+1))
         return -1 
-
+                    
 
