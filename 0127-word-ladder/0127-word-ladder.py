d3d02d6d3a5049ded4293bc7223c3cfa4da52497
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-03-20 20:28:09 -0400

    Time: 54 ms (95.35%), Space: 21.3 MB (25.62%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,6 +1,5 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
         if endWord not in wordList:
             return 0
         
@@ -8,21 +7,19 @@ class Solution:
         wordList.append(beginWord)
 
         for word in wordList:
-            for j in range(len(word)):
-                key = word[0:j] + "*" + word[j+1:]
+            for i in range(len(word)):
+                key = word[0:i] + "*" + word[i+1:]
                 graph[key].append(word)
-
-        seen = {beginWord}
+        
         queue = deque([(beginWord,1)])
+        seen = {beginWord}
 
         while queue:
-            word, steps = queue.popleft()
-
+            word,steps = queue.popleft()
             if word == endWord:
                 return steps
-
-            for j in range(len(word)):
-                key = word[0:j] + "*" + word[j+1:]
+            for i in range(len(word)):
+                key = word[0:i] + "*" + word[i+1:]
                 for neigh in graph[key]:
                     if neigh not in seen:
                         seen.add(neigh)
