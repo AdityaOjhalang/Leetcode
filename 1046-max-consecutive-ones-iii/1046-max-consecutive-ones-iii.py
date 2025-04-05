c5f49b57cd180f2262f271b771c286a37a27bbd4
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-05 10:19:24 -0700

    Time: 63 ms (66.33%), Space: 18.3 MB (81.71%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -8,5 +8,5 @@ class Solution:
                 if nums[l] == 0:
                     zeros -= 1
                 l += 1
-                ans = max(ans, r - l + 1)
+            ans = max(ans, r - l + 1)
         return ans
