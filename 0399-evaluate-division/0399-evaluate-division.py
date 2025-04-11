428a55a0c3b7b5763c6049a41816f5fe978de924
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-11 16:24:41 -0400

    Time: 0 ms (100%), Space: 18.1 MB (8.46%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,30 +1,27 @@
 class Solution:
-    def calcEquation(
-        self, equations: List[List[str]], values: List[float], queries: List[List[str]]
-    ) -> List[float]:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
         graph = defaultdict(list)
-
-        for i, eq in enumerate(equations):
-            a, b = eq
-            graph[a].append((b, values[i]))
-            graph[b].append((a, 1 / values[i]))
+        for i,eq in enumerate(equations):
+            a,b = eq
+            graph[a].append((b,values[i]))
+            graph[b].append((a,1/values[i]))
         
         def bfs(src,dst):
             if src not in graph or dst not in graph:
                 return -1
-            queue,seen = deque([(src,1)]), {src}
+            queue = deque([(src,1)])
+            seen = {src}
             while queue:
                 node,ratio = queue.popleft()
                 if node == dst:
                     return ratio
-                
                 for neigh,weight in graph[node]:
                     if neigh not in seen:
                         seen.add(neigh)
                         queue.append((neigh,weight*ratio))
             return -1
-            
         res = []
-        for x, y in queries:
-            res.append(bfs(x, y))
-        return res
+        for x,y in queries:
+            res.append(bfs(x,y))
+        return res 
