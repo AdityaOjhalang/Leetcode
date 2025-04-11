bb5ef16961e3483ad90e726479637a95541d7766
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-11 16:09:05 -0400

    Time: 192 ms (59.66%), Space: 51.8 MB (41.99%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -2,22 +2,20 @@ class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         oldroads = set()
         graph = defaultdict(list)
-
+        
         for x,y in connections:
             graph[x].append(y)
             graph[y].append(x)
             oldroads.add((x,y))
-
-        self.reorder = 0
+        
+        self.res = 0
         def dfs(node):
             for neigh in graph[node]:
                 if neigh not in seen:
                     if (node,neigh) in oldroads:
-                        self.reorder += 1
+                        self.res += 1
                     seen.add(neigh)
                     dfs(neigh)
         seen = {0}
         dfs(0)
-        return self.reorder
-        
-
+        return self.res
