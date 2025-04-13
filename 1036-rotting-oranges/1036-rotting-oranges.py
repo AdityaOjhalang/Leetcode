1305116054f2162551ca49efbfd3011c8d0c559d
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-13 15:04:34 -0400

    Time: 7 ms (27.22%), Space: 17.7 MB (93.46%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,37 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        ROWS, COLS = len(grid), len(grid[0])
-        FRESH, ROTTEN = 1, 2
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        totalfresh = 0
+        m,n = len(grid),len(grid[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        fresh = 0
+        FRESH,ROTTEN = 1,2
         queue = deque()
 
-        def valid(row, col):
-            return 0 <= row < ROWS and 0 <= col < COLS
-
-        for i in range(ROWS):
-            for j in range(COLS):
+        def valid(r,c):
+            return 0<=r<m and 0<=c<n and grid[r][c] == FRESH
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == ROTTEN:
-                    queue.append((i, j))
+                    queue.append((i,j))
                 if grid[i][j] == FRESH:
-                    totalfresh += 1
-
-        if totalfresh == 0:
+                    fresh += 1
+        time = -1
+        if fresh == 0:
             return 0
-        nummins = -1
+
         while queue:
-            nummins += 1
             tot = len(queue)
+            time += 1
             for _ in range(tot):
-                row, col = queue.popleft()
-                for x, y in directions:
-                    nr, nc = row + x, col + y
-                    if valid(nr,nc) and grid[nr][nc] == FRESH:
-                        grid[nr][nc] = ROTTEN
+                row,col = queue.popleft()
+                for x,y in directions:
+                    nr,nc = row+x,col+y
+                    if valid(nr,nc):
+                        grid[nr][nc] = ROTTEN 
+                        fresh -= 1
                         queue.append((nr,nc))
-                        totalfresh -= 1
-        if totalfresh == 0:
-            return nummins
+        if fresh == 0:
+            return time
         else:
             return -1
