590226cd7b28f975a9126984e204079a331af95f
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-13 14:43:43 -0400

    Time: 122 ms (22.49%), Space: 20 MB (35.92%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -1,24 +1,31 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        ROWS,COLS = len(maze),len(maze[0])
-        directions =  [(0,1),(1,0),(-1,0),(0,-1)]
-        s_row,s_col = entrance
-        queue = deque([(s_row,s_col,0)])
-        seen = {(s_row,s_col)}
-        
-        def valid(r,c):
-            return 0 <= r < ROWS and 0 <= c < COLS and maze[r][c] == "." and (r,c) not in seen
-        
-        def checkend(r,c):
-            return (r in {0,ROWS-1} or c in {0,COLS-1}) and (r,c) != (s_row,s_col) 
-        
+        ROWS, COLS = len(maze), len(maze[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        s_row, s_col = entrance
+        seen = {(s_row, s_col)}
+        queue = deque([(s_row, s_col, 0)])
+
+        def valid(r, c):
+            return (
+                0 <= r < ROWS
+                and 0 <= c < COLS
+                and (r, c) not in seen
+                and maze[r][c] == "."
+            )
+
+        def checkend(r, c):
+            return (r in {0, ROWS - 1} or c in {0, COLS - 1}) and (r, c) != (
+                s_row,
+                s_col,
+            )
 
         while queue:
-            row,col,steps = queue.popleft()
-            if checkend(row,col):
+            row, col, steps = queue.popleft()
+            if checkend(row, col):
                 return steps
-            for x,y in directions:
-                nr,nc = row+x,col+y
+            for x, y in directions:
+                nr, nc = row + x, col + y
                 if valid(nr,nc):
                     seen.add((nr,nc))
                     queue.append((nr,nc,steps+1))
