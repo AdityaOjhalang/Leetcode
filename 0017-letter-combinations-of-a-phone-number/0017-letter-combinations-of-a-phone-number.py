f0a59ee71d953ed2d829e2447fa03c16dc9cd32e
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-16 12:10:22 -0400

    Time: 0 ms (100%), Space: 17.9 MB (23.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,5 +1,8 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+
         letters = {
             "2": "abc",
             "3": "def",
@@ -10,17 +13,18 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
-        if len(digits) == 0:
-            return []
+
         res = []
         def backtrack(path,i):
             if len(path) == len(digits):
                 res.append("".join(path))
-                return
+                return 
             possibles = letters[digits[i]]
             for char in possibles:
                 path.append(char)
                 backtrack(path,i+1)
                 path.pop()
         backtrack([],0)
-        return res
+        return res 
+
+
