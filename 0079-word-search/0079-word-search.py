c1adf93721aa8a6d9b5c31df64e9ba06a43cdfa5
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-17 12:51:44 -0400

    Time: 5402 ms (22.09%), Space: 17.9 MB (64.76%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,26 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS = len(board)
-        COLS = len(board[0])
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
-        def valid(row,col):
-            return 0<=row<ROWS and 0<=col<COLS
-        
-        def backtrack(row,col,i,seen):
+        m, n = len(board), len(board[0])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        def valid(r, c):
+            return 0 <= r < m and 0 <= c < n 
+
+        def backtrack(row, col, i, seen):
             if i == len(word):
                 return True
-            
-            for x,y in directions:
-                nr,nc = row+x,col+y
-                if valid(nr,nc) and board[nr][nc] == word[i] and (nr,nc) not in seen:
-                    seen.add((nr,nc))
-                    if backtrack(nr,nc,i+1,seen):
+
+            for x, y in directions:
+                nr, nc = row + x, col + y
+                if valid(nr, nc) and (nr, nc) not in seen and board[nr][nc] == word[i]:
+                    seen.add((nr, nc))
+                    if backtrack(nr, nc, i + 1, seen):
                         return True
-                    seen.remove((nr,nc))
+                    seen.remove((nr, nc))
             return False
-        
-        for row in range(ROWS):
-            for col in range(COLS):
-                if board[row][col] == word[0] and backtrack(row,col,1,{(row,col)}):
+
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] == word[0] and backtrack(row, col, 1, {(row, col)}):
                     return True
         return False
-
-                        
