888ce8ed4bb7f08654868d8355fe990bdb12823e
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-19 14:15:11 -0400

    Time: 3 ms (73.38%), Space: 20.7 MB (7.03%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,5 +4,5 @@ class Solution:
         def dp(i):
             if i == 0 or i == 1:
                 return 0
-            return min(cost[i-1]+dp(i-1),cost[i-2]+dp(i-2))
+            return min(dp(i-1) + cost[i-1] , dp(i-2) + cost[i-2])
         return dp(len(cost))
