1b53fabe75506cdd829a1beaf638ed7fe0221e1a
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-20 13:17:13 -0400

    Time: 210 ms (5.44%), Space: 130.7 MB (11.39%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -4,6 +4,6 @@ class Solution:
         def dp(i):
             if i >= len(questions):
                 return 0
-            return max(questions[i][0] + dp(i + questions[i][1] + 1), dp(i+1))
-        
+            points,brainpower = questions[i]
+            return max(dp(i+1), points + dp(i+brainpower + 1))
         return dp(0)
