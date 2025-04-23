a15cec41116f5eaf140f415d9f94ca6ca9c54084
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-23 16:38:06 -0400

    Time: 13 ms (59.49%), Space: 19.3 MB (40.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,13 +5,12 @@ class Solution:
         l,r = 0,n-1
         water = 0
         while l < r:
-            if lmax < rmax :
-                l+=1
+            if lmax < rmax:
+                l += 1
                 lmax = max(lmax,height[l])
                 water += lmax - height[l]
             else:
                 r -= 1
                 rmax = max(rmax,height[r])
                 water += rmax - height[r]
-        return water 
-                
+        return water
