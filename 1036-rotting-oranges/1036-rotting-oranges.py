371e3043400b83a91de02e7c39cf4271bc426f32
Aditya Ojha <63713668+AdityaOjhalang@users.noreply.github.com>
2025-04-24 15:24:55 -0400

    Time: 7 ms (27.13%), Space: 18 MB (27.74%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,35 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m,n = len(grid),len(grid[0])
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        m, n = len(grid), len(grid[0])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         fresh = 0
-        FRESH,ROTTEN = 1,2
         queue = deque()
+        FRESH, ROTTEN = 1, 2
 
         def valid(r,c):
             return 0<=r<m and 0<=c<n and grid[r][c] == FRESH
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == ROTTEN:
-                    queue.append((i,j))
+                    queue.append((i, j))
                 if grid[i][j] == FRESH:
                     fresh += 1
-        time = -1
+                    
         if fresh == 0:
             return 0
 
+        time = -1
         while queue:
             tot = len(queue)
             time += 1
             for _ in range(tot):
-                row,col = queue.popleft()
-                for x,y in directions:
-                    nr,nc = row+x,col+y
+                row, col = queue.popleft()
+                for x, y in directions:
+                    nr, nc = row + x, col + y
                     if valid(nr,nc):
-                        grid[nr][nc] = ROTTEN 
-                        fresh -= 1
+                        grid[nr][nc] = ROTTEN
                         queue.append((nr,nc))
+                        fresh -= 1
+            
+        
         if fresh == 0:
             return time
         else:
             return -1
+    
+
